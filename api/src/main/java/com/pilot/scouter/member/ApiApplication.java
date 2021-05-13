ecb2f8b28cb12b014ea02535a4a527e65a7c4214
anisia20 <anisia1124@gmail.com>
2021-05-13 14:21:05 +0900

    first id

--- api/src/main/java/com/pilot/scouter/member/ApiApplication.java
+++ api/src/main/java/com/pilot/scouter/member/ApiApplication.java
@@ -56,7 +56,7 @@ public class ApiApplication {
         //최초 아이디 admin admin
         ClientDto tmp = new ClientDto();
         tmp.setId("admin");
-        tmp.setPwd(ADMSHA512Hash.getDigest("admin"));
+        tmp.setPwd("c7ad44cbad762a5da0a452f9e854fdc1e0e7a52a38015f23f3eab1d80b931dd472634dfac71cd34ebc35d16ab7fb8a90c81f975113d6c7538dc69dd8de9077ec");
         redisCmd.hput(RedisConstants.SCOUTER_H_CLIENT.key, "admin", tmp);
 
         try {
